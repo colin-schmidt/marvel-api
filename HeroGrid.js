e81d8568d627fd1c690c354b6c4675bf39f87b9c
Colin Schmidt <75905471+colin-schmidt@users.noreply.github.com>
2021-05-29 10:26:28 -0500

    Update HeroGrid.js

--- HeroGrid.js
+++ HeroGrid.js
@@ -1,39 +1,33 @@
 import "./HeroGrid.css";
 import React from "react";
 
-class HeroGrid extends React.Component {
-  render() {
-    if (!this.props.characterName) {
-      return null;
-    }
+const HeroGrid = ({ characterName }) => {
+  const apiCall = characterName.data.results[0];
 
-    const apiCall = this.props.characterName.data.results[0];
-
-    return (
-      <div className="ui link cards">
-        <div className="ui card">
-          <div className="image">
-            <img
-              alt="heroimg"
-              src={`${apiCall.thumbnail.path}.${apiCall.thumbnail.extension}`}
-            />
+  return !characterName ? null : (
+    <div className="ui link cards">
+      <div className="ui card">
+        <div className="image">
+          <img
+            alt="heroimg"
+            src={`${apiCall.thumbnail.path}.${apiCall.thumbnail.extension}`}
+          />
+        </div>
+        <div className="content">
+          <a className="ui header massive">{apiCall.name}</a>
+          <div className="meta">
+            <span className="date">
+              Has appeared in {apiCall.comics.available} comic books{" "}
+            </span>
           </div>
-          <div className="content">
-            <a className="ui header massive">{apiCall.name}</a>
-            <div className="meta">
-              <span className="date">
-                Has appeared in {apiCall.comics.available} comic books{" "}
-              </span>
-            </div>
-            <div className="description">
-              {apiCall.description ? apiCall.description : ""}
-            </div>
-            <button className="ui button red">Remove</button>
+          <div className="description">
+            {apiCall.description ? apiCall.description : ""}
           </div>
+          <button className="ui button red">Remove</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HeroGrid;
