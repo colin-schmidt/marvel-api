558634fea54a32f2d6ab733e3be91d4cb818ded6
Colin Schmidt <colincarlschmidt@gmail.com>
2021-08-30 16:42:33 -0500

    Added remove button functionality to HeroGrid

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import HeroGrid from "./components/HeroGrid";
+import FailedSearch from "./components/FailedSearch";
+
+const App = () => {
+  const [character, setCharacter] = useState("");
+
+  const onSearchSubmit = async (input) => {
+    const { data } = await axios.get(
+      "https://gateway.marvel.com/v1/public/characters",
+      {
+        params: {
+          name: input,
+          apikey: "745d1cce0c4bb368d4a02bf9a9b33cc9",
+        },
+      }
+    );
+    setCharacter(data);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={onSearchSubmit} />
+      <HeroGrid characterName={character} setCharacter={setCharacter} />
+      <FailedSearch characterName={character} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HeroGrid.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../HeroGrid.css";
+
+const HeroGrid = ({ characterName, setCharacter }) => {
+  const apiCall = !characterName ? null : characterName.data.results[0];
+
+  return !characterName ? null : (
+    <div className="ui link cards">
+      <div className="ui card">
+        <div className="image">
+          <img
+            alt="heroimg"
+            src={`${apiCall.thumbnail.path}.${apiCall.thumbnail.extension}`}
+          />
+        </div>
+        <div className="content">
+          <a className="ui header massive">{apiCall.name}</a>
+          <div className="meta">
+            <span className="date">
+              Has appeared in {apiCall.comics.available} comic books{" "}
+            </span>
+          </div>
+          <div className="description">
+            {apiCall.description ? apiCall.description : "No bio available."}
+          </div>
+          <button className="ui button red" onClick={() => setCharacter(null)}>
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroGrid;
