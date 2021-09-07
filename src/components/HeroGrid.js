bfe095ccedab8045f2f6c4f07fc4bcd2fefc335b
Colin Schmidt <colincarlschmidt@gmail.com>
2021-09-07 15:04:31 -0500

    map take 2

--- src/components/HeroGrid.js
+++ src/components/HeroGrid.js
@@ -2,38 +2,40 @@ import React from "react";
 import "./HeroGrid.css";
 
 const HeroGrid = ({ character, setCharacter }) => {
-
-  const apiResults = character == false ? null : character.data.results[0];
-
-  if (apiResults) {
-    return (
-      <div className="ui link cards">
-        <div className="ui card">
-          <div className="image">
-            <img
-              alt="heroimg"
-              src={`${apiResults.thumbnail.path}.${apiResults.thumbnail.extension}`}
-            />
-          </div>
-          <div className="content">
-            <a className="ui header massive">{apiResults.name}</a>
-            <div className="appeared">
-              <span>
-                📚Has appeared in {apiResults.comics.available} comic books
-              </span>
+  if (character.length > 0) {git stat
+    return character.map((arr) => (
+      <div className="ui grid">
+        <div className="ui link cards" key={arr.name}>
+          <div className="ui card">
+            <div className="image">
+              <img
+                alt="heroimg"
+                src={`${arr.thumbnail.path}.${arr.thumbnail.extension}`}
+              />
             </div>
-            <div className="heroBio">
-              {apiResults.description
-                ? `🦸‍♂️${apiResults.description}`
-                : "🦸‍♂️No bio available."}
+            <div className="content">
+              <a className="ui header massive">{arr.name}</a>
+              <div className="appeared">
+                <span>
+                  📚Has appeared in {arr.comics.available} comic books
+                </span>
+              </div>
+              <div className="heroBio">
+                {arr.description
+                  ? `🦸‍♂️${arr.description}`
+                  : "🦸‍♂️No bio available."}
+              </div>
+              <button
+                className="ui button red"
+                onClick={() => setCharacter([])}
+              >
+                Remove hero
+              </button>
             </div>
-            <button className="ui button red" onClick={() => setCharacter([])}>
-              Remove hero
-            </button>
           </div>
         </div>
       </div>
-    );
+    ));
   } else return null;
 };
 
